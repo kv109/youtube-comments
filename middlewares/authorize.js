eff1da0f923a02add71866dd3589985b2bf6a014
Kacper Walanus <kacper@walanus.com>
2017-08-07 13:43:32 +0200

    extract authenticate with access token

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,14 +1,12 @@
 const youtube = require("youtube-api");
+const authenticateWithAccessToken = require(servicesDir + "oauth2").authenticateWithAccessToken;
 
 const authorizeWithAccessToken = (req, res, next) => {
   const accessToken = req.session && req.session.tokens && req.session.tokens.access_token;
   const redirectToSignInPage = () => res.redirect('/');
 
   if (accessToken) {
-    youtube.authenticate({
-      type: "oauth",
-      token: accessToken
-    });
+    authenticateWithAccessToken(accessToken);
     next();
   } else {
     redirectToSignInPage();
--- services/oauth2.js
+++ services/oauth2.js
@@ -1,11 +1,21 @@
 const YT_CREDENTIALS = require(rootDir + 'config/youtube_credentials.json');
 const Youtube = require("youtube-api");
 
-let oauth = Youtube.authenticate({
-  type: "oauth",
-  client_id: YT_CREDENTIALS.web.client_id,
-  client_secret: YT_CREDENTIALS.web.client_secret,
-  redirect_url: YT_CREDENTIALS.web.redirect_uris[0]
-});
+const oauth = {
+  oauth: Youtube.authenticate({
+    type: "oauth",
+    client_id: YT_CREDENTIALS.web.client_id,
+    client_secret: YT_CREDENTIALS.web.client_secret,
+    redirect_url: YT_CREDENTIALS.web.redirect_uris[0]
+  }),
+
+  authenticateWithAccessToken: (accessToken) => {
+    Youtube.authenticate({
+      type: "oauth",
+      token: accessToken
+    });
+  }
+};
+
 
 module.exports = oauth;
