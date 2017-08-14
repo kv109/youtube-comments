e3d841490a1adcf5855e431b8331e91d2ad2d9f7
Kacper Walanus <kacper@walanus.com>
2017-08-14 17:47:15 +0200

    refactor

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const app = express();
-const server = require('http').createServer(app);
+// express stack
+const express = require('express')
+  , path = require('path')
+  , logger = require('morgan')
+  , cookieParser = require('cookie-parser')
+  , bodyParser = require('body-parser')
+  , app = express()
+  , server = require('http').createServer(app);
+
+// load ENV vars from .env file
 require("dotenv").config();
 
+// Dirs are stored in global vars
 global.rootDir = path.resolve(__dirname) + '/';
 global.middlewaresDir = path.join(rootDir, 'middlewares/');
+global.routesDir = path.join(rootDir, 'routes/');
 global.servicesDir = path.join(rootDir, 'services/');
 
 // Websockets
+const io = require('socket.io')(server); // Initialize io here and pass it wherever needed.
 const authorizeWithJwt = require(middlewaresDir + "/sockets/authorize");
-const io = require('socket.io')(server);
 io.use(authorizeWithJwt);
 
-// Routes
-const index = require('./routes/index');
-const comments = require('./routes/comments')(io);
-
-// view engine setup
+// Template engine
 app.set('views', path.join(rootDir, 'views'));
 app.set('view engine', 'pug');
 
-// session
+// Session
 app.use(require(middlewaresDir + "/session"));
 
-// flash messages
+// Flash messages
 app.use(require('express-flash-messages')());
 
+// Basic express stack
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -39,9 +40,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(rootDir, 'public')));
 
+// Routes
+// root path
 app.get('/', (req, res) => res.redirect('/sign_in'));
-app.use('/', index);
-app.use('/comments', comments);
+app.use('/', require(routesDir + 'index'));
+app.use('/comments', require(routesDir + 'comments')(io));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
